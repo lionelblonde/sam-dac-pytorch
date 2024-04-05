96401c79f7b8efe12bbf1921cca4e93acad13644
lionelblonde <lionel.blonde@me.com>
2024-04-05 23:41:35 +0200

    Add new launchers and edit existing ones

--- m2_launcher.sh
+++ m2_launcher.sh
@@ -2,15 +2,15 @@
 
 python main.py \
     --wandb_project ava \
-    --env_id Walker-2d \
+    --env_id Walker2d-v4 \
     --seed 0 \
-    --cuda \
-    --fp16 \
+    --no-cuda \
+    --no-fp16 \
     --no-mps \
     --no-render \
     --no-record \
     --task train \
-    --num_timesteps 1e7 \
+    --num_timesteps 10000000 \
     --training_steps_per_iter 4 \
     --eval_steps_per_iter 10 \
     --eval_every 10 \
@@ -24,7 +24,7 @@ python main.py \
     --rollout_len 2 \
     --batch_size 128 \
     --gamma 0.99 \
-    --mem_size 1e5 \
+    --mem_size 100000 \
     --noise_type "adaptive-param_0.2" \
     --pn_adapt_frequency 50. \
     --polyak 0.005 \
@@ -33,11 +33,11 @@ python main.py \
     --lookahead 5 \
     --no-ret_norm \
     --no-popart \
-    --expert_path "/Users/lionelblonde/Datasets/mujoco-experts" \
+    --expert_path "/Users/lionelblonde/Datasets/mujoco-experts/Walker2d-v4" \
     --num_demos 4 \
     --g_steps 3 \
     --d_steps 1 \
-    --d_lr 1e-5 \
+    --d_lr 0.00001 \
     --no-state_only \
     --minimax_only \
     --ent_reg_scale 0.001 \
